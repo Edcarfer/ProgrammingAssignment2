b7471278b9d6e3528b03310715b6b3e6e9cd3eee
Edcarfer <edgarcf87@gmail.com>
2021-12-10 09:40:19 +0100

    Commit of the assignment, second try because of SSH

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,37 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Couple of functions that create a special "Matrix" object, which is a list
+## containing functions for setting and getting the matrix, and its inverse
+## and another function which obtains the inverse if not cached already.
+## This is an assignment of Coursera Course, so please follow the Honor Code.
+### As a possible improvement, the union of both functions in a special object
+### could be interesting.
 
-## Write a short comment describing this function
+## Creation of a special "Matrix" object.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setinv <- function(inv) m <<- inv
+  getinv <- function() m
+  list(set = set, get = get,
+       setinv = setinv,
+       getinv = getinv)
 }
 
 
-## Write a short comment describing this function
+## Solve matrix function, which retrieves the cached value if it exists
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  inv <- x$getinv()
+  if(!is.null(inv)) {
+    message("getting cached data")
+    return(inv)
+  }
+  data <- x$get()
+  inv <- solve(data, ...)
+  x$setinv(inv)
+  inv
 }
